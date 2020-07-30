61ad5650038c51051cd5d4302cb84837203ed863
shareed <shanreed6@gmail.com>
2020-07-30 10:34:07 -0700

    finished navigation

--- learning-app/pages/notes/[id].js
+++ learning-app/pages/notes/[id].js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 //params Catch-all routes
 export default () => {
   const router = useRouter()
   const { id }= router.query
 
   return (
+    <div>
     <h1>Note: {id} </h1>
+    <Link href="/notes">
+        <a>Notes</a>
+      </Link>
+      </div>
   )
 }
